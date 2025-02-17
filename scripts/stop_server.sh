004e14e03bf2bebcc9b73ea78ccffb41294f205f
Daniel Nataren <daniel_nataren@outlook.com>
2025-02-17 16:32:04 -0600

    yml added

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,11 @@
 git pull origin master
 
-cp ./ /var/www/html/ -r
+if [ -f /var/www/html/] then
+    sudo rm -r /var/www/html/
+fi
+
+sudo cp ./ /var/www/html/ -r
+
+sudo mv /var/www/html/index.html /var/www/html/index.nginx-debian.html
 
 sudo service nginx restart
