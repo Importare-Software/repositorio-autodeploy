0889a82e4ba028660072c7ced8c92a76980bcb2e
Daniel Nataren <daniel_nataren@outlook.com>
2025-02-18 13:55:39 -0600

    ssh test

--- stop_server.sh
+++ stop_server.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# 1) Set HOME explicitly so 'git config --global' knows where to write the config.
 export HOME=/home/ubuntu
 
-# 2) Mark the repository as safe to avoid the 'dubious ownership' warning.
 git config --global --add safe.directory /home/ubuntu/Docker/Proyectos/prueba/repositorio-autodeploy
 
-# Proceed with the rest of the commands
-pwd
 cd /home/ubuntu/Docker/Proyectos/prueba/repositorio-autodeploy
-pwd
 
-# Discard local changes
 git checkout .
 
-# Pull latest changes from remote
 git pull
