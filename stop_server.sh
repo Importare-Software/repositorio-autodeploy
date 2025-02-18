61f9f815e5a7216285902b92cc8f1c7939d43691
Daniel Nataren <daniel_nataren@outlook.com>
2025-02-18 11:27:50 -0600

    ssh test

--- stop_server.sh
+++ stop_server.sh
@@ -1,1 +1,12 @@
-pwd
+git checkout master
+git pull origin master
+
+# sudo rm -rf /var/www/html/*
+
+# sudo cp ./ /var/www/html/ -r
+
+# sudo mv /var/www/html/index.html /var/www/html/index.nginx-debian.html
+
+# sudo service nginx restart
+
+touch hola.txt
