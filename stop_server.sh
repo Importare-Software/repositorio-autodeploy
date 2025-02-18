e7b68e1ac9a1689c8e76636fac029fa5a67ba0e2
Daniel Nataren <daniel_nataren@outlook.com>
2025-02-18 11:10:18 -0600

    ssh test

--- stop_server.sh
+++ stop_server.sh
@@ -3,12 +3,12 @@ cd ~/Docker/Proyectos/prueba/repositorio-autodeploy
 git checkout master
 git pull origin master
 
-sudo rm -rf /var/www/html/*
+# sudo rm -rf /var/www/html/*
 
-sudo cp ./ /var/www/html/ -r
+# sudo cp ./ /var/www/html/ -r
 
-sudo mv /var/www/html/index.html /var/www/html/index.nginx-debian.html
+# sudo mv /var/www/html/index.html /var/www/html/index.nginx-debian.html
 
-sudo service nginx restart
+# sudo service nginx restart
 
 touch hola.txt
